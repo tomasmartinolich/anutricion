fc87536e295b2366d386a378bcb3996bab0b123f
tomasmartinolich <54339205+tomasmartinolich@users.noreply.github.com>
2022-04-20 17:10:10 -0300

    v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 // componentes
 import { AppComponent } from './app.component';
-import { ConsultorioComponent } from './pages/consultorio/consultorio.component';
 import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
-import { TrabajoInstitucionalComponent } from './pages/trabajo-institucional/trabajo-institucional.component';
 import { CursosComponent } from './pages/cursos/cursos.component';
 import { ContactanosComponent } from './pages/contactanos/contactanos.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { TrabajoComponent } from './pages/trabajo/trabajo.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'quienes-somos', component: QuienesSomosComponent },
-  { path: 'consultorio', component: ConsultorioComponent },
-  { path: 'trabajo-institucional', component: TrabajoInstitucionalComponent },
+  { path: 'trabajo', component: TrabajoComponent },
   { path: 'cursos', component: CursosComponent },
   { path: 'contactanos', component: ContactanosComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,23 @@ import { HeaderComponent } from './components/header/header.component';
 //paginas
 import { AppComponent } from './app.component';
 import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
-import { ConsultorioComponent } from './pages/consultorio/consultorio.component';
-import { TrabajoInstitucionalComponent } from './pages/trabajo-institucional/trabajo-institucional.component';
 import { CursosComponent } from './pages/cursos/cursos.component';
 import { ContactanosComponent } from './pages/contactanos/contactanos.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { TrabajoComponent } from './pages/trabajo/trabajo.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuienesSomosComponent,
-    ConsultorioComponent,
-    TrabajoInstitucionalComponent,
     CursosComponent,
     ContactanosComponent,
     HeaderComponent,
-    InicioComponent
+    InicioComponent,
+    TrabajoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
